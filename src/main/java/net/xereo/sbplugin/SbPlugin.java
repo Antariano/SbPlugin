2f2e036294b22d88a1247ea2c56765be54e26b76
LimeByte <limebyte@twf.co.nz>
2013-06-13 16:55:40 +1200

    Use proper Java conventions.

--- src/main/java/net/xereo/sbplugin/SbPlugin.java
+++ src/main/java/net/xereo/sbplugin/SbPlugin.java
@@ -2,12 +2,10 @@ package net.xereo.sbplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class SbPlugin extends JavaPlugin
-{
+public class SbPlugin extends JavaPlugin {
 
     @Override
-    public void onEnable()
-    {
+    public void onEnable() {
         // TODO Everything!
     }
 
